bc469b4cc81b22a105f4b78992aa37433bb108f0
xiaolin <siaolin8284@gmail.com>
2024-05-20 15:49:58 +0800

    refactor: 修正 Player.php 檔案中的檔案名稱和變數名稱並刪除不必要的程式碼
    因應需求做了以下調整：
    1. 修正檔案內已更改過的檔案名稱，將 require_once conn.php 改為 require_once connect.php；
    2. 將變數名稱從 conn 修正為 connect；
    3. 修改 Class Player 中的方法名稱，將 createCharacter 改為 create。
    4. 刪除不必要的註解及程式碼

--- game/Class/Player.php
+++ game/Class/Player.php
@@ -1,48 +1,33 @@
 <?php
-require_once "conn.php";
+require_once "connect.php";
 
 class Player {
-    private $conn;
+    private $connect;
     public $level;
     public $experience;
 
     // 建構函數：設置資料庫連接並初始化等級為1等、經驗值為0
-    public function __construct($conn) {
-        $this->conn = $conn;
+    public function __construct($connect) {
+        $this->connect = $connect;
         $this->level = 1;
         $this->experience = 0;
     }
 
     // 創建角色方法
-    public function createCharacter($playerName, $jobChoice, $jobName, $healthPoints, $attackPoints, $defensePoints, $magicPoints, $magicAttackPoints, $magicDefensePoints, $luckPoints) {
+    public function create($playerName, $jobChoice, $jobName, $healthPoints, $attackPoints, $defensePoints, $magicPoints, $magicAttackPoints, $magicDefensePoints, $luckPoints) {
         // SQL 查詢
         $sql = "INSERT INTO players (player_Name, job_id, job_name, player_level, player_experience) VALUES ('$playerName', '$jobChoice', '$jobName', '$this->level', '$this->experience');";
         $sql .= "INSERT INTO characters (health_points, attack, defense, magic_points, magic_attack, magic_defense, luck) VALUES ('$healthPoints', '$attackPoints', '$defensePoints', '$magicPoints', '$magicAttackPoints', '$magicDefensePoints', '$luckPoints');";
 
-        // 打印 SQL 查询语句
-        //echo "SQL 查询语句：" . $sql;
-
         // 執行 SQL 查詢
-        if ($this->conn->multi_query($sql) === TRUE) {
+        if ($this->connect->multi_query($sql) === TRUE) {
             // 成功建立角色
             return true;
         } else {
-            echo "Error: " . $sql . "<br>" . $this->conn->error;
+            echo "Error: " . $sql . "<br>" . $this->connect->error;
             // 發生錯誤
             return false;
         }
     }
-    // //玩家隨機攻擊8-20傷害
-    // public function attack() {
-    //     return rand(8, 20);
-    // }
-
-    // //升級方法
-    // public function levelUp($completedLevels) {
-    //     //完成當前關卡即升一級
-    //     if ($completedLevels > $this->level) {
-    //         $this->level = $completedLevels;
-    //         echo "恭喜您升等了！目前等級為: " . $this->level . "\n";
-    //     }
-    // }
+    
 }
