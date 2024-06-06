805691aaab95eb93698da7c5b0fbe80f0a8e0f25
xiaolin <siaolin8284@gmail.com>
2024-06-06 15:53:06 +0800

    feat: 新增 Game 類別，修改遊戲迴圈邏輯及處理退出遊戲選項

--- game/index.php
+++ game/index.php
@@ -4,10 +4,12 @@ require_once "connect.php";
 require_once "Class/MainMenu.php";
 require_once "Class/Character.php";
 require_once "Class/Player.php";
+require_once "Class/Game.php";
 
 // 初始化遊戲
 $mainMenu = new MainMenu();
 $createCharacter = new Character($connect);
+$game = new Game('凜凜仔的遊戲');
 
 // 顯示遊戲主選單
 $mainMenu->showMainMenu();
@@ -17,10 +19,17 @@ while (TRUE) {
     // 取得玩家輸入選擇
     $choice = $mainMenu->playerChoice();
     // 處理玩家的選擇
-    if ($choice === '1') {
-        // 開始遊戲
-    } elseif ($choice ==='2') {
+    if ($choice === '1' || $choice === '2') {
         // 創建角色
-        $createCharacter->createCharacter($connect);
+        $playerName = $createCharacter->createCharacter($connect);
+        $game->addPlayer($playerName);
+        sleep(2);
+        // 開始遊戲
+        $game->start();
+        // 開始遊戲後即退出迴圈
+        break;
+    } elseif ($choice ==='4') {
+        echo "感謝你的遊玩～再見\n";
+        exit();
     }
 }
