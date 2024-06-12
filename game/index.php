386e4a2fb8d8f240f6734561ba5af7bfe69dedf6
xiaolin <siaolin8284@gmail.com>
2024-06-12 15:54:57 +0800

    feat: 新增GameRecord類別管理遊戲紀錄，並調整參數

--- game/index.php
+++ game/index.php
@@ -1,15 +1,16 @@
 <?php
-// 引入文件
 require_once "connect.php";
 require_once "Class/MainMenu.php";
 require_once "Class/Character.php";
 require_once "Class/Player.php";
 require_once "Class/Game.php";
+require_once "Class/GameRecord.php";
 
 // 初始化遊戲
 $mainMenu = new MainMenu();
-$createCharacter = new Character($connect);
-$game = new Game('凜凜仔的遊戲');
+$createCharacter = new Character();
+$game = new Game($createCharacter, $connect);
+$gameRecord = new GameRecord($connect);
 
 // 顯示遊戲主選單
 $mainMenu->showMainMenu();
@@ -21,14 +22,19 @@ while (TRUE) {
     // 處理玩家的選擇
     if ($choice === '1' || $choice === '2') {
         // 創建角色
-        $playerName = $createCharacter->createCharacter($connect);
+        $playerName = $createCharacter->createCharacter();
         $game->addPlayer($playerName);
         sleep(2);
         // 開始遊戲
         $game->start();
-        // 開始遊戲後即退出迴圈
-        break;
+    } elseif ($choice === '3') {
+        // 查看遊戲紀錄
+        $gameRecord->displayGameRecord();
+        // 再次顯示主選單
+        $mainMenu->showMainMenu();
     } elseif ($choice ==='4') {
+        echo "即將結束遊戲！";
+        sleep(1);
         echo "感謝你的遊玩～再見\n";
         exit();
     }
